359ead30d6b040edf483d9213c549f0b8805182d
elkham07 <elkhammamedov@gmail.com>
2026-02-01 18:40:21 +0500

    Add core models based on ERD

---
+++ internal/models/models.go
@@ -0,0 +1,26 @@
+package models
+
+import "time"
+
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Role     string `json:"role"`
+}
+
+type Product struct {
+	ID            int     `json:"id"`
+	Name          string  `json:"name"`
+	Price         float64 `json:"price"`
+	CategoryID    int     `json:"category_id"`
+	StockQuantity int     `json:"stock_quantity"`
+}
+
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
